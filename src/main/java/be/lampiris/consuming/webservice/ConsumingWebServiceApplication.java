a284535c571582cc45b1b46779c8c234e45b07bb
rufin.hounkpe <rufin.hounkpe@lampiris.be>
2021-05-25 19:34:04 +0200

    Initial state

---
+++ src/main/java/be/lampiris/consuming/webservice/ConsumingWebServiceApplication.java
@@ -0,0 +1,13 @@
+package be.lampiris.consuming.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/lampiris/consuming/webservice/ConsumingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.lampiris.consuming.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
